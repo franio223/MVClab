57f5bda151e50a13862d6a4461d3a31de617c956
Kacper Franczyk <kacper.franczyk23@gmail.com>
2025-04-25 14:58:05 +0200

    koniec części 4

--- django/djangotutorial/mysite/settings.py
+++ django/djangotutorial/mysite/settings.py
@@ -55,10 +55,11 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [],  
+        'APP_DIRS': True,  
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -66,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
